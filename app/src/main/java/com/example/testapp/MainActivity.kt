6493ecd27d020a2e2d191764b8cd39a1298df90b
Furkan <mfurkanargunsah@gmail.com>
2022-10-09 21:07:28 +0300

    databinding test

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.testapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var design:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      design =  DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        design.sonuc.text = "0"
+
+        design.topla.setOnClickListener{
+
+            val input1 = design.number1.text.toString()
+            val input2 = design.number2.text.toString()
+
+            val number1 = input1.toInt()
+            val number2 = input2.toInt()
+
+            val total = number1 + number2
+
+            design.sonuc.text = total.toString()
+
+            design.number1.text.clear()
+            design.number2.text.clear()
+
+        }
+
+        design.carp.setOnClickListener{
+
+            val input1 = design.number1.text.toString()
+            val input2 = design.number2.text.toString()
+
+            val number1 = input1.toInt()
+            val number2 = input2.toInt()
+
+            val total = number1 * number2
+
+            design.sonuc.text = total.toString()
+
+            design.number1.text.clear()
+            design.number2.text.clear()
+
+
+        }
+
     }
 }
